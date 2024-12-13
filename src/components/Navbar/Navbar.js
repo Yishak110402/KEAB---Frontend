8ccfb1b8a78bc32c445663c673502c447d86abb0
yishak <yishak.110402@gmail.com>
2024-12-13 17:23:45 +0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -37,7 +37,10 @@ export default function Navbar() {
           <NavLink onClick={()=>(setNavOpen(false))} to="/aboutus">About us</NavLink>
           <NavLink onClick={()=>(setNavOpen(false))} to='/team'>Team</NavLink>
           <NavLink onClick={()=>(setNavOpen(false))} to='/projects'>Projects</NavLink>
+          <NavLink onClick={()=>(setNavOpen(false))} to='/news'>News</NavLink>
+          <NavLink onClick={()=>(setNavOpen(false))} to='/events'>Events</NavLink>
           <NavLink onClick={()=>(setNavOpen(false))} to='/contact'>Contact</NavLink>
+      
         </ul>
         <div className="hamburger">
         <svg onClick={()=>(setNavOpen((n)=>(!n)))} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
