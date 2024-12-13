d6466e1bb832e0b6779b659c53ab8c4f184c7acf
yishak <yishak.110402@gmail.com>
2024-12-13 13:21:42 +0300

    first commit

--- src/components/Homepage/Hero/Hero.js
+++ src/components/Homepage/Hero/Hero.js
@@ -6,7 +6,7 @@ export default function Hero(){
                 <h1>Welcome to KEABI Africa Community Development and Human Rights Organization (KACDHRO)</h1>
             </div>
             <div className="hero-image">
-                <img src="https://i.postimg.cc/qgVp1QtT/new-keab-hero.jpg" alt="KEAB Logo" />
+                <img src="https://i.postimg.cc/qgVp1QtT/new-keab-hero.jpg" alt="KEAB Logo" loading="lazy"/>
             </div>
         </div>
     )
