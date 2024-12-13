b0e9321bef2830feadc8818b62814d3ccd456aef
yishak <yishak.110402@gmail.com>
2024-12-13 15:10:57 +0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { GeneralContext } from "../../hooks/GeneralContext";
 
 export default function Navbar() {
   const{navOpen, setNavOpen} = useContext(GeneralContext)
+  useEffect(function(){
+    if (navOpen){
+      document.body.style.overflow = 'hidden'
+    }else{
+      document.body.style.overflow = 'visible'
+    }
+    return ()=>{
+      document.body.style.overflow = 'visible'
+    }
+  },[navOpen])
   return (
     <>
       <div className="info">
@@ -13,7 +23,7 @@ export default function Navbar() {
           <p>Email: keabafrica@gmail.com</p>
         </div>
       </div>
-      <nav className={`navbar ${!navOpen ? "closed" : ""}`}>
+      <nav className={`navbar ${navOpen ? "" : "closed"}`}>
         <div className="logo">
           <Link to="/">
             <img
@@ -30,7 +40,7 @@ export default function Navbar() {
           <NavLink to='/contact'>Contact</NavLink>
         </ul>
         <div className="hamburger">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        <svg onClick={()=>(setNavOpen((n)=>(!n)))} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
         </div>
       </nav>
     </>
