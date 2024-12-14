b8d64c1ad2254a7350d2b32de3b73026c8318817
yishak <yishak.110402@gmail.com>
2024-12-14 19:26:14 +0300

    first commit

--- src/pages/OpenEvent.js
+++ src/pages/OpenEvent.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import EventContent from "../components/OpenEvent/EventContent/EventContent";
 
 export default function OpenEvent(){
+    useEffect(function(){
+        window.scrollTo({
+            top:0,
+            behavior:"instant"
+        })
+    },[])
     return(
         <>
             <EventContent />
