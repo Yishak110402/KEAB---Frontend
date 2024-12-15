5f1354b93a862f0e593096760c67776e4be66450
yishak <yishak.110402@gmail.com>
2024-12-15 19:33:16 +0300

    first commit

--- src/components/OpenTeam/OpenTeamContainer/OpenTeamContainer.js
+++ src/components/OpenTeam/OpenTeamContainer/OpenTeamContainer.js
@@ -1,11 +1,18 @@
-import { useParams } from "react-router-dom"
-import "./OpenTeamContainer.css"
+import { useParams } from "react-router-dom";
+import "./OpenTeamContainer.css";
+import { GeneralContext } from "../../../hooks/GeneralContext";
+import { useContext } from "react";
 
-export default function OpenTeamContainer(){
-    const {id} = useParams()
-    return(
-        <div className="open-team-container">
-            <h1>{id}</h1>
-        </div>
-    )
+export default function OpenTeamContainer() {
+  const { id } = useParams();
+  const splitName = id.split("-");
+  const name = splitName[0] + " " + splitName[1];
+  const { team } = useContext(GeneralContext);
+  const selectedMember = team.filter((t) => t.name.includes(name))[0];
+  return (
+    <div className="open-team-container">
+      <h1>{selectedMember.name}</h1>
+      
+    </div>
+  );
 }
--- src/components/Team/Member/Member.js
+++ src/components/Team/Member/Member.js
@@ -2,8 +2,9 @@ import { useNavigate } from "react-router-dom"
 import "./Member.css"
 export default function Member({team, index}){
     const navigate = useNavigate()    
+    const splitName = team.name.split(" ")
     return(
-        <div onClick={()=>(navigate(`/team/${index}`))} className="member">
+        <div onClick={()=>(navigate(`/team/${splitName[0]}-${splitName[1]}`))} className="member">
             <img src={team.image} alt="" />
             <h3>{team.name}</h3>
             <h4>{team.position}</h4>
