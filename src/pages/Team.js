23481fe844049c15050f73244c7f33977e696ac6
yishak <yishak.110402@gmail.com>
2024-12-15 18:40:19 +0300

    first commit

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import TeamContainer from "../components/Team/TeamContainer/TeamContainer";
 import TeamHeader from "../components/Team/TeamHeader/TeamHeader";
 
 export default function Team(){
+    useEffect(function(){
+        window.scrollTo({
+            top:0,
+            behavior:"instant"
+        })
+    },[])
     return(
         <>
             <TeamHeader />
