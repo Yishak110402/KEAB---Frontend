9d56b65da3299a73e8507a8e109714374ed9ab8d
yishak <yishak.110402@gmail.com>
2024-12-15 18:37:28 +0300

    first commit

--- src/components/Team/Member/Member.js
+++ src/components/Team/Member/Member.js
@@ -3,6 +3,9 @@ import "./Member.css"
 import { GeneralContext } from "../../../hooks/GeneralContext"
 export default function Member(){
     const {team} = useContext(GeneralContext)
+    const execTeam = team.filter((t)=>(t.team === "executive"))
+    console.log(execTeam);
+    
     return(
         <div className="member">
             <img src="https://i.postimg.cc/s2vXWD1W/natnael-ashenafi-bio.jpg" alt="" />
