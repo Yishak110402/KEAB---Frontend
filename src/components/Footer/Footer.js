3bddaa27261190d2457aeb8e32525d08cdd82cdd
yishak <yishak.110402@gmail.com>
2024-12-29 09:39:37 +0300

    first commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,9 @@ import './Footer.css'
 export default function Footer(){
     return(
         <footer className="footer">
-            
+            <div className='logo'>
+                
+            </div>
         </footer>
     )
 }
