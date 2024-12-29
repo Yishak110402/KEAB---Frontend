8d482b184082cd49aacf1eb7a889602b8b2cb5c4
yishak <yishak.110402@gmail.com>
2024-12-29 11:35:45 +0300

    first commit

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import EventsContainer from "../components/Events/EventsContainer/EventsContainer";
 import EventsHeader from "../components/Events/EventsHeader/EventsHeader";
 
 export default function Events(){
+    useEffect(function(){
+        window.scrollTo({
+            top:0,
+            behavior:"instant"
+        })
+    },[])
     return(
         <>
             <EventsHeader />
